3495d95261217cfb1261f470ae7a0023d01ad332
Admin <Admin@DESKTOP-0BVTUIG>
2024-11-16 17:20:05 +0300

    Добавьте файлы проекта.

---
+++ HomeWork10.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Input words");
+
+        string input = Console.ReadLine();
+
+        string result = input.Replace(" ", "");
+        
+        Console.WriteLine(result);
+    }
+}
